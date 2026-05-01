04fad6a00ffabea46de7fd06e1d5cd61caef5dfa
atshaw1994 <atshaw1994@gmail.com>
2026-05-01 15:25:35 -0400

    Fix audio references

--- _Scripts/AudioManager.gd
+++ _Scripts/AudioManager.gd
@@ -2,12 +2,12 @@ extends Node
 
 # Dictionary to store the preloaded sounds for instant playback[cite: 6]
 var sounds = {
-	"button_click": preload("res://_Audio/button_click.wav"),
-	"harvest": preload("res://_Audio/harvest.wav"),
-	"hoe": preload("res://_Audio/hoe.wav"),
-	"plant_seed": preload("res://_Audio/plant_seed.wav"),
-	"purchase": preload("res://_Audio/purchase.wav"),
-	"sell": preload("res://_Audio/sell.wav")
+	"button_click": preload("res://_audio/button_click.wav"),
+	"harvest": preload("res://_audio/harvest.wav"),
+	"hoe": preload("res://_audio/hoe.wav"),
+	"plant_seed": preload("res://_audio/plant_seed.wav"),
+	"purchase": preload("res://_audio/purchase.wav"),
+	"sell": preload("res://_audio/sell.wav")
 }
 
 # Pool of players to handle overlapping sounds (e.g., clicking fast)
